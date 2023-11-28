d24f1d84d79ba3e178294e790d3096392b73ce37
Samkelwa <samkelwa101@gmail.com>
2023-11-28 23:40:43 +0200

    added some text content, some more work needs to be done.   2023/11/28    23:40

---
+++ dynamic to-do list/script.js
@@ -0,0 +1,42 @@
+console.log("Dynamic To-Do List") ;
+
+// 1. Create the HTML structure for the to-do list app. Include an input field, a submit button, and an empty list element:
+
+
+// 2. Create a JavaScript function to add a new task to the list:
+function addTask() {
+    let taskInput = document.getElementById("taskInput") ;
+    let taskList = document.getElementById("taskList") ;
+
+    if (taskInput.value.trim() !== "") {
+        let listItem = document.createElement("li") ;
+
+        let checkBox = document.createElement("input") ;
+        checkBox.type = "checkbox"
+
+        let taskText = document.createTextNode(taskInput.value) ;
+
+        let deleteButton = document.createElement("button") ;
+        deleteButton.textContent = "Delete" ;
+
+        listItem.appendChild(checkBox) ;
+        listItem.appendChild(taskText) ;
+        listItem.appendChild(deleteButton) ;
+
+        taskList.appendChild(listItem) ;
+
+        taskInput.value = "" ;
+    }
+}
+
+/*
+This function first checks if the input field is not empty.
+If it’s not, the function creates a new list item 
+with a checkbox, the task text, and a delete button, then appends it to the task list.
+Finally, it clears the input field.
+*/
+
+
+// 3. Add an event listener to the “Add Task” button to call the addTask() function when clicked:
+let addButton = document.getElementById("addTask") ;
+addButton.addEventListener("click", addTask) ;
